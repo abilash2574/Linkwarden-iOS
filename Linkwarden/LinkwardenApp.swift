bee0bdeb5670d1704afad99b5e86130b0b9ac75b
Abilash S <abilash.suresh@zohocorp.com>
2024-03-09 11:50:12 +0530

    Initial Commit

---
+++ Linkwarden/LinkwardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkwardenApp.swift
+//  Linkwarden
+//
+//  Created by Abilash S on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LinkwardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
